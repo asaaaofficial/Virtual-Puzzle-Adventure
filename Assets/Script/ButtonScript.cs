ac9e671812c8ec943ac9438f2d763301e8dda09f
unknown <asaaaofficial.com>
2025-05-21 11:13:30 +0700

    Update Project

--- Assets/Script/ButtonScript.cs
+++ Assets/Script/ButtonScript.cs
@@ -1,47 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
-using static UnityEngine.InputSystem.HID.HID;
+using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int pindahScene = 1;
 
-    public UnityEngine.UI.Button myButton;
-    public UnityEngine.UI.Button confirmButton;
-    void Start()
+    public void ChangeScene()
     {
-        if (myButton != null)
-        {
-            myButton.onClick.AddListener(OnButtonClick);
-        }
-        else
-        {
-            Debug.LogWarning("Button belum di-assign di Inspector!");
-        }
-
-        if (confirmButton != null)
-        {
-            confirmButton.onClick.AddListener(EndPuzzle);
-        }
-        else
-        {
-            Debug.LogWarning("confirmButton belum di-assign di Inspector!");
-        }
-    }
-
-    // Update is called once per frame
-
-    void OnButtonClick()
-    {
-        Debug.Log("Tombol diklik!");
-    }
-
-    void EndPuzzle()
-    {
-        Debug.Log("Puzzle selesai! Tombol Confirm ditekan.");
+        SceneManager.LoadScene(pindahScene);
     }
     void Update()
     {
---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,56 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void ButtonScript_ChangeScene_m79F120F52DD08A330F5B7F45946B57A06A6C824B (void);
+extern void ButtonScript_Update_mC431CC6750CD25E81253BED68877702C3228F8C7 (void);
+extern void ButtonScript__ctor_m8877AA5F1151123B36B4513BD26E3547494C3FB8 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	ButtonScript_ChangeScene_m79F120F52DD08A330F5B7F45946B57A06A6C824B,
+	ButtonScript_Update_mC431CC6750CD25E81253BED68877702C3228F8C7,
+	ButtonScript__ctor_m8877AA5F1151123B36B4513BD26E3547494C3FB8,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	7648,
+	7648,
+	7648,
+	11582,
+	7648,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
